a83206c56db574bb8d5bf26ecdd664dd96411573
Your Name <chen@krasus.red>
2024-09-25 13:59:06 +0800

    格式化代码

--- services/mq_service.py
+++ services/mq_service.py
@@ -14,6 +14,8 @@ from models import SunoJobTypeEnum
 from services.music_service import MusicService
 from suno.entities import ClipStatusEnum
 logger = logging.getLogger(__name__)
+
+
 class MQSerivce():
 
     def consume(self):
@@ -25,7 +27,7 @@ class MQSerivce():
             # Schedule the two consuming functions to run in separate threads
             loop.run_in_executor(executor, self.start_consuming)
             loop.run_in_executor(executor, self.start_music_consuming)
-            
+
             # Run the event loop
             loop.run_forever()
 
@@ -35,27 +37,27 @@ class MQSerivce():
         service = LyricsService()
         db = next(get_db())
         data = json.loads(body)
-        job = service.create(data,db)
-        task_type = data.get('type','lyrics')
+        job = service.create(data, db)
+        task_type = data.get('type', 'lyrics')
         task_id = data.get('id')
-        
+
         if job.job_id:
             # 说明任务添加成功
             job_result = None
             for _ in range(0, 10):
                 time.sleep(2)
-                job_result = service.get(job.id,db)
+                job_result = service.get(job.id, db)
                 if job_result.status == ClipStatusEnum.ERROR.value or job_result.status == ClipStatusEnum.COMPLETE.value:
                     break
             # public job.response to public queue
             data = json.dumps({
-                "id":task_id,
+                "id": task_id,
                 "type": task_type,
                 "response": job.to_json()
-            },ensure_ascii=True).encode('utf-8')
+            }, ensure_ascii=True).encode('utf-8')
             rabbitmq.get_public_channel().basic_publish(
-                exchange='', 
-                routing_key=config.RABBITMQ_PUBLIC_QUEUE, 
+                exchange='',
+                routing_key=config.RABBITMQ_PUBLIC_QUEUE,
                 body=data)
             # 任务成功，发送ack
             ch.basic_ack(delivery_tag=method.delivery_tag)
@@ -64,37 +66,37 @@ class MQSerivce():
             # 任务失败，重试
             logger.debug('MQService: Received unknown request')
             ch.basic_nack(delivery_tag=method.delivery_tag)
-    
+
     @staticmethod
     def gen_music(ch, method, properties, body):
         service = MusicService()
         db = next(get_db())
         data = json.loads(body)
         request = data.get("prompt")
-        task_type = data.get('type','lyrics')
+        task_type = data.get('type', 'lyrics')
         id = data.get('id')
         try:
 
             # 查询db，查看当前account在运行的任务数量
-            running_jobs = service.get_running_jobs(job_type=SunoJobTypeEnum.MUSIC, db=db)
+            running_jobs = service.get_running_jobs(
+                job_type=SunoJobTypeEnum.MUSIC, db=db)
 
             if len(running_jobs) >= config.SUNO_MAX_RUNNING_JOBS:
                 logger.debug('MQService: Reached max running jobs')
                 raise Exception("Reached max running jobs")
 
-
-            jobs = service.create(request,db)
+            jobs = service.create(request, db)
 
             # 发布任务到 suno music queue
             for job in jobs:
                 data = json.dumps({
-                    "id":id,
+                    "id": id,
                     "type": task_type,
                     "job_id": str(job.id)
-                },ensure_ascii=True).encode('utf-8')
+                }, ensure_ascii=True).encode('utf-8')
                 rabbitmq.get_public_music_channel().basic_publish(
-                    exchange='', 
-                    routing_key=config.RABBITMQ_PUBLIC_MUSIC_QUEUE, 
+                    exchange='',
+                    routing_key=config.RABBITMQ_PUBLIC_MUSIC_QUEUE,
                     body=data
                 )
             # 任务成功，返回ack
@@ -102,25 +104,24 @@ class MQSerivce():
             ch.basic_ack(delivery_tag=method.delivery_tag)
         except Exception as ex:
             logger.error(f"任务添加失败，id={id} ,type={task_type} ,ex={str(ex)}")
-            time.sleep(5) # 失败稍等在执行
-            ch.basic_nack(delivery_tag=method.delivery_tag,requeue=True)
-        
+            time.sleep(5)  # 失败稍等在执行
+            ch.basic_nack(delivery_tag=method.delivery_tag, requeue=True)
 
     def start_consuming(self):
         logger.info("MQService: Start consuming")
         # self.consume_channel = rabbitmq.get_consume_channel()
+
         def callback(ch, method, properties, body):
             logger.info(f"Received message: {body}")
             data = json.loads(body)
-            task_type = data.get('type','lyrics')
+            task_type = data.get('type', 'lyrics')
 
             if task_type == 'lyrics':
                 MQSerivce.gen_lyrics(ch, method, properties, body)
             else:
                 MQSerivce.gen_music(ch, method, properties, body)
 
-
-        self.consume_channel.basic_consume(queue=config.RABBITMQ_CONSUME_QUEUE, 
+        self.consume_channel.basic_consume(queue=config.RABBITMQ_CONSUME_QUEUE,
                                            on_message_callback=callback,
                                            auto_ack=False,
                                            consumer_tag=config.RABBITMQ_CONSUME_TAG)
@@ -132,6 +133,7 @@ class MQSerivce():
             try:
                 self.music_channel = rabbitmq.get_public_music_channel()
                 # self.consume_channel = rabbitmq.get_consume_channel()
+
                 def music_callback(ch, method, properties, body):
                     logger.info(f"Received message: {body}")
                     data = json.loads(body)
@@ -141,34 +143,37 @@ class MQSerivce():
                     service = MusicService()
                     jobs = service.mq_fetch_suno(db)
                     try:
-                        job = service.get(data.get('job_id'),db)
-                        if job and ( job.status == ClipStatusEnum.ERROR.value or job.status == ClipStatusEnum.COMPLETE.value):
+                        job = service.get(data.get('job_id'), db)
+                        if job and (job.status == ClipStatusEnum.ERROR.value or job.status == ClipStatusEnum.COMPLETE.value):
                             data = json.dumps({
-                                "id":data.get('id'),
+                                "id": data.get('id'),
                                 "type": data.get('type'),
                                 "response": job.to_json()
-                            },ensure_ascii=True).encode('utf-8')
+                            }, ensure_ascii=True).encode('utf-8')
                             rabbitmq.get_public_channel().basic_publish(
-                                exchange='', 
-                                routing_key=config.RABBITMQ_PUBLIC_QUEUE, 
+                                exchange='',
+                                routing_key=config.RABBITMQ_PUBLIC_QUEUE,
                                 body=data
                             )
                             ch.basic_ack(delivery_tag=method.delivery_tag)
                         else:
-                            ch.basic_nack(delivery_tag=method.delivery_tag,requeue=True )
+                            ch.basic_nack(
+                                delivery_tag=method.delivery_tag, requeue=True)
                             time.sleep(10)
                     except Exception as ex:
-                        logger.error(f"任务失败，id={data.get('id')} ,type={data.get('type')} ,ex={str(ex)}")
-                        ch.basic_nack(delivery_tag=method.delivery_tag,requeue=True )
+                        logger.error(
+                            f"任务失败，id={data.get('id')} ,type={data.get('type')} ,ex={str(ex)}")
+                        ch.basic_nack(
+                            delivery_tag=method.delivery_tag, requeue=True)
                         time.sleep(10)
 
-                self.music_channel.basic_consume(queue=config.RABBITMQ_PUBLIC_MUSIC_QUEUE, 
+                self.music_channel.basic_consume(queue=config.RABBITMQ_PUBLIC_MUSIC_QUEUE,
                                                  on_message_callback=music_callback,
                                                  auto_ack=False,
                                                  consumer_tag=config.RABBITMQ_PUBLIC_MUSIC_CONSUME_TAG
                                                  )
                 self.music_channel.start_consuming()
-            except (pika.exceptions.ConnectionClosedByBroker, 
+            except (pika.exceptions.ConnectionClosedByBroker,
                     pika.exceptions.AMQPChannelError,
                     pika.exceptions.AMQPConnectionError) as e:
                 logger.error(f"Connection error: {e}")
@@ -180,7 +185,6 @@ class MQSerivce():
                 time.sleep(5)  # 等待5秒后重试连接
             finally:
                 pass
-
 
     def stop_consuming(self):
         if self.consume_channel:
